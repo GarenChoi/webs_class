ec128e1418ad610cc2c6cb52574389f8be0e5144
1 <97070162+GarenChoi@users.noreply.github.com>
2022-02-21 16:53:31 +0900

    2022/02/21

---
+++ javascript/effect/js/quiz.js
@@ -0,0 +1,31 @@
+//tabmenu
+const tabBtn = document.querySelectorAll(".menu-bar > span");
+const tabCont = document.querySelectorAll(".content > .main");
+
+tabBtn.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+
+        tabBtn.forEach(el => {
+            el.classList.remove("active");
+        })
+
+        btn.classList.add("active");
+
+        tabCont.forEach(div => {
+            div.style.display = "none";
+        })
+
+        tabCont[index].style.display = "block";
+    });
+});
+
+//소스 버튼
+const modalA = document.querySelector("#modal");
+const openBtn = document.querySelector(".source .quiz__source");
+const closeBtn = document.querySelector(".title-bar .close");
+openBtn.addEventListener("click", () => {
+    modalA.style.display = "block";
+});
+closeBtn.addEventListener("click", () => {
+    modalA.style.display = "none";
+});
