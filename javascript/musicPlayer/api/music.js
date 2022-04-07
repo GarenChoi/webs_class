fe978a85c4bf656d8b858c8837dd00fac96ef11c
GarenChoi <jundan2010@gmail.com>
2022-04-07 21:51:49 +0900

    2022/04/07

---
+++ javascript/musicPlayer/api/music.js
@@ -0,0 +1,98 @@
+let allMusic = [
+    {
+        name : "clear",
+        artist : "Jone Mickal",
+        img : "music-1",
+        audio : "music-1"
+    },
+    {
+        name : "Everybody Knows",
+        artist : "Leonard Cohen",
+        img : "music-2",
+        audio : "music-2"
+    },
+    {
+        name : "Extreme Ways",
+        artist : "Moby",
+        img : "music-3",
+        audio : "music-3"
+    },
+    {
+        name : "Butterflies",
+        artist : "Sia",
+        img : "music-4",
+        audio : "music-4"
+    },
+    {
+        name : "Fuck You",
+        artist : "Last Child",
+        img : "music-5",
+        audio : "music-5"
+    },
+    {
+        name : "Genius",
+        artist : "LSD",
+        img : "music-6",
+        audio : "music-6"
+    },
+    {
+        name : "Comeback Kid",
+        artist : "Lindi",
+        img : "music-7",
+        audio : "music-7"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-8",
+        audio : "music-8"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-9",
+        audio : "music-9"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-10",
+        audio : "music-10"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-11",
+        audio : "music-11"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-12",
+        audio : "music-12"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-13",
+        audio : "music-13"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-14",
+        audio : "music-14"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-15",
+        audio : "music-15"
+    },
+    {
+        name : "노래 제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-16",
+        audio : "music-16"
+    }
+]
