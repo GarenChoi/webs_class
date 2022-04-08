0e715ff523430b6256851de6d8ddcf31b4c33244
GarenChoi <jundan2010@gmail.com>
2022-04-08 14:15:55 +0900

    2022/04/08

--- javascript/musicPlayer/api/music.js
+++ javascript/musicPlayer/api/music.js
@@ -1,96 +1,96 @@
 let allMusic = [
     {
-        name : "clear",
+        name : "1. clear",
         artist : "Jone Mickal",
         img : "music-1",
         audio : "music-1"
     },
     {
-        name : "Everybody Knows",
+        name : "2. Everybody Knows",
         artist : "Leonard Cohen",
         img : "music-2",
         audio : "music-2"
     },
     {
-        name : "Extreme Ways",
+        name : "3. Extreme Ways",
         artist : "Moby",
         img : "music-3",
         audio : "music-3"
     },
     {
-        name : "Butterflies",
+        name : "4. Butterflies",
         artist : "Sia",
         img : "music-4",
         audio : "music-4"
     },
     {
-        name : "Fuck You",
+        name : "5. Fuck You",
         artist : "Last Child",
         img : "music-5",
         audio : "music-5"
     },
     {
-        name : "Genius",
+        name : "6. Genius",
         artist : "LSD",
         img : "music-6",
         audio : "music-6"
     },
     {
-        name : "Comeback Kid",
+        name : "7. Comeback Kid",
         artist : "Lindi",
         img : "music-7",
         audio : "music-7"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "8. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-8",
         audio : "music-8"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "9. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-9",
         audio : "music-9"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "10. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-10",
         audio : "music-10"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "11. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-11",
         audio : "music-11"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "12. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-12",
         audio : "music-12"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "13. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-13",
         audio : "music-13"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "14. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-14",
         audio : "music-14"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "15. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-15",
         audio : "music-15"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "16. 노래 제목 - 제목",
         artist : "Jone Mickal",
         img : "music-16",
         audio : "music-16"
